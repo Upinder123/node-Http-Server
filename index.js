e98b75f9bdf537156b52aa293840369bbcb652c2
upinder <upinderjits@gmail.com>
2020-05-09 12:42:27 +0530

    support for public folder,support for JSON

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const fs = require('fs');
 
 const server = http.createServer((req,res)=>{
     if(req.url === '/'){
-        res.end('<h1>Home<h1>');
+        fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+            if (err) throw err;
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.end(content);
+            
+        })
+    }else if(req.url === '/api/users'){
+        //support for JSON
+        const users = [
+            {name:'Bob Smith', age: 40},
+            {name: 'John Doe', age: 30}
+        ];
+        res.writeHead(200,{'Content-Type':'application/json'});
+        res.end(JSON.stringify(users));
     }
+    
 });
 
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
 
+ 
